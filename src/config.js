f6d7e940c7dff7690ab741d605be59afc1ff562a
Denis <dmarkitanov@gmail.com>
2019-10-05 11:53:03 +0600

    refactoring

--- src/config.js
+++ src/config.js
@@ -1,44 +1,44 @@
-'use strict';
-const fs = require('fs');
-const os = require('os');
-const path = require('path');
-const pkg = require('../package.json');
+'use strict'
+const fs = require('fs')
+const os = require('os')
+const path = require('path')
+const pkg = require('../package.json')
 
-const {join} = path;
-const {default: defaultConfig} = pkg.configuration;
+const { join } = path
+const { default: defaultConfig } = pkg.configuration
 
 class Config {
-  constructor() {
-    this._configFile = join(os.homedir(), '.taskbook.json');
+  constructor () {
+    this._configFile = join(os.homedir(), '.taskbook.json')
 
-    this._ensureConfigFile();
+    this._ensureConfigFile()
   }
 
-  _ensureConfigFile() {
+  _ensureConfigFile () {
     if (fs.existsSync(this._configFile)) {
-      return;
+      return
     }
 
-    const data = JSON.stringify(defaultConfig, null, 4);
-    fs.writeFileSync(this._configFile, data, 'utf8');
+    const data = JSON.stringify(defaultConfig, null, 4)
+    fs.writeFileSync(this._configFile, data, 'utf8')
   }
 
-  _formatTaskbookDir(path) {
-    return join(os.homedir(), path.replace(/^~/g, ''));
+  _formatTaskbookDir (path) {
+    return join(os.homedir(), path.replace(/^~/g, ''))
   }
 
-  get() {
-    let config = {};
+  get () {
+    let config = {}
 
-    const content = fs.readFileSync(this._configFile, 'utf8');
-    config = JSON.parse(content);
+    const content = fs.readFileSync(this._configFile, 'utf8')
+    config = JSON.parse(content)
 
     if (config.taskbookDirectory.startsWith('~')) {
-      config.taskbookDirectory = this._formatTaskbookDir(config.taskbookDirectory);
+      config.taskbookDirectory = this._formatTaskbookDir(config.taskbookDirectory)
     }
 
-    return Object.assign({}, defaultConfig, config);
+    return Object.assign({}, defaultConfig, config)
   }
 }
 
-module.exports = new Config();
+module.exports = new Config()
--- src/item.js
+++ src/item.js
@@ -1,16 +1,16 @@
-'use strict';
+'use strict'
 
-const now = new Date();
+const now = new Date()
 
 class Item {
-  constructor(options = {}) {
-    this._id = options.id;
-    this._date = now.toDateString();
-    this._timestamp = now.getTime();
-    this.description = options.description;
-    this.isStarred = options.isStarred || false;
-    this.boards = options.boards || [];
+  constructor (options = {}) {
+    this._id = options.id
+    this._date = now.toDateString()
+    this._timestamp = now.getTime()
+    this.description = options.description
+    this.isStarred = options.isStarred || false
+    this.boards = options.boards || []
   }
 }
 
-module.exports = Item;
+module.exports = Item
